ec30be4079a0e79d5dfe753ea3e1273c119463d8
SurajPatel04 <surajpatel9390@gmail.com>
2025-05-28 00:10:21 +0530

    cors added

--- backendAiInterview/src/app.js
+++ backendAiInterview/src/app.js
@@ -7,13 +7,26 @@ dotenv.config({
 });
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://ai-interview-r4kaoxqra-surajpatel04s-projects.vercel.app',
+  'https://ai-interview-git-main-surajpatel04s-projects.vercel.app',
+  'https://ai-interview-seven-chi.vercel.app'
+];
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: false
-  })
-);
+app.use(cors({
+  origin: function(origin, callback){
+    // Allow requests with no origin (like Postman or curl)
+    if(!origin) return callback(null, true);
+    
+    if(allowedOrigins.indexOf(origin) !== -1){
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
  
 app.use(express.json());
 app.use(express.urlencoded({extended: true, limit:"16kb"}));
