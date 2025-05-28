dc36c0636a1b4edcfa237a8c254f8099ed378484
SurajPatel04 <surajpatel9390@gmail.com>
2025-05-29 02:40:47 +0530

    test

--- frontendAiInterview/src/main.jsx
+++ frontendAiInterview/src/main.jsx
@@ -73,6 +73,8 @@ createRoot(document.getElementById("root")).render(
   </StrictMode>,
 );
 
+// TEST ONLY
+
 // createRoot(document.getElementById('root')).render(
 //   <StrictMode>
 //     <App />
