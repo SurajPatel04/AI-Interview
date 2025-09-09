b812fb7a2b6ef810969ad6cf637655e3ab5e6dd3
SurajPatel <surajpatel9390@gmail.com>
2025-09-09 20:19:11 +0530

    add: ngnix.conf file

--- frontendAiInterview/vite.config.js
+++ frontendAiInterview/vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: 'http://localhost:8000/',
+          target: 'http://backend:8000/',
           changeOrigin: true,
           secure: true,
           // Remove /api prefix if your backend doesn’t use it
